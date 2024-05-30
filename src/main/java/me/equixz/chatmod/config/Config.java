632abe91575d09e1ea06bf4e8782ad89fcf61abe
Equixz <mads.leisten@hotmail.com>
2024-05-30 22:30:10 +0200

    something

--- src/main/java/me/equixz/chatmod/config/Config.java
+++ src/main/java/me/equixz/chatmod/config/Config.java
@@ -10,7 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-@SuppressWarnings("ALL")
+@SuppressWarnings("CallToPrintStackTrace")
 public class Config {
 
     public static class ConfigData {
